a6d8868c1b1e2c670fe38d412d4599e9c37955c3
Rudolf Polzer <rpolzer@google.com>
2020-12-24 21:44:40 -0500

    Add some basic tiles.

---
+++ assets/tiles/tiles.tsx
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tiles" tilewidth="16" tileheight="16" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="empty">
+  <image width="16" height="16" source="empty.png"/>
+ </tile>
+ <tile id="1" type="solid">
+  <image width="16" height="16" source="wall00.png"/>
+ </tile>
+ <tile id="2" type="solid">
+  <image width="16" height="16" source="wall01.png"/>
+ </tile>
+ <tile id="3" type="solid">
+  <image width="16" height="16" source="wall02.png"/>
+ </tile>
+ <tile id="4" type="solid">
+  <image width="16" height="16" source="wall03.png"/>
+ </tile>
+ <tile id="5" type="solid">
+  <image width="16" height="16" source="wall04.png"/>
+ </tile>
+ <tile id="6" type="solid">
+  <image width="16" height="16" source="wall05.png"/>
+ </tile>
+ <tile id="7" type="solid">
+  <image width="16" height="16" source="wall06.png"/>
+ </tile>
+ <tile id="8" type="solid">
+  <image width="16" height="16" source="wall07.png"/>
+ </tile>
+ <tile id="9" type="solid">
+  <image width="16" height="16" source="wall08.png"/>
+ </tile>
+ <tile id="10" type="solid">
+  <image width="16" height="16" source="wall09.png"/>
+ </tile>
+ <tile id="11" type="solid">
+  <image width="16" height="16" source="wall10.png"/>
+ </tile>
+ <tile id="12" type="solid">
+  <image width="16" height="16" source="wall11.png"/>
+ </tile>
+ <tile id="13" type="solid">
+  <image width="16" height="16" source="wall12.png"/>
+ </tile>
+ <tile id="14" type="solid">
+  <image width="16" height="16" source="wall13.png"/>
+ </tile>
+ <tile id="15" type="solid">
+  <image width="16" height="16" source="wall14.png"/>
+ </tile>
+ <tile id="16" type="solid">
+  <image width="16" height="16" source="wall15.png"/>
+ </tile>
+ <wangsets>
+  <wangset name="Wall/Solid" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x2020202"/>
+   <wangtile tileid="2" wangid="0x2020201"/>
+   <wangtile tileid="3" wangid="0x1020202"/>
+   <wangtile tileid="4" wangid="0x1020201"/>
+   <wangtile tileid="5" wangid="0x2020102"/>
+   <wangtile tileid="6" wangid="0x2020101"/>
+   <wangtile tileid="7" wangid="0x1020102"/>
+   <wangtile tileid="8" wangid="0x1020101"/>
+   <wangtile tileid="9" wangid="0x2010202"/>
+   <wangtile tileid="10" wangid="0x2010201"/>
+   <wangtile tileid="11" wangid="0x1010202"/>
+   <wangtile tileid="12" wangid="0x1010201"/>
+   <wangtile tileid="13" wangid="0x2010102"/>
+   <wangtile tileid="14" wangid="0x2010101"/>
+   <wangtile tileid="15" wangid="0x1010102"/>
+   <wangtile tileid="16" wangid="0x1010101"/>
+  </wangset>
+ </wangsets>
+</tileset>
