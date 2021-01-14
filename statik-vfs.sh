17f054293fe8af908051ba84b8e271e883b7e1f5
Rudolf Polzer <rpolzer@google.com>
2021-01-14 10:32:41 -0500

    Copy 3p licenses into statik.

--- statik-vfs.sh
+++ statik-vfs.sh
@@ -19,5 +19,18 @@ else
 			ln -snf "$root/$sourcedir/$file" "$tmpdir/$file"
 		done
 	done
+	cd "$root"
+	for license in third_party/*/LICENSE; do
+		directory=${license%/*}
+		directory=${directory#third_party/}
+		cd "$root/third_party/$directory"
+		[ -d assets ] || continue
+		{
+			echo "Applying to the following files:"
+			find assets -type f -print
+			echo
+			cat LICENSE
+		} > "$tmpdir/$directory.LICENSE"
+	done
 fi
 statik -m -f -src "$tmpdir/" -dest "$root/$destdir"
