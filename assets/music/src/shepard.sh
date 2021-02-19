98859a9884d41616f8c8f0a6cb0f2708d5d88b02
Rudolf Polzer <rpolzer@google.com>
2021-02-19 14:01:57 -0800

    Add TEH SHEPARD TONE.

---
+++ assets/music/src/shepard.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Generates "Shepard tone" audio files.
+
+center=0
+basevol=0.5
+falloff=0.95
+attack=0.01
+decay=2.95
+len=3
+for basenote in $(seq 0 11); do
+	set --
+	vols=
+	ch=0
+	for i in $(seq -32 32); do
+		note=$((basenote + i * 12))
+		if [ $note -lt -48 ] || [ $note -gt 39 ]; then
+			continue
+		fi
+		delta=$((note - center))
+		delta=${delta#-}
+		vol=$(echo "$falloff ^ $delta" | bc -l)
+		ch=$((ch+1))
+		vols=$vols${ch}v$vol,
+		set -- "$@" "$len" triangle "%$note"
+	done
+	sox -n -r 48k -e signed -b 16 -c 1 $(printf ../shepard_%02d.wav $basenote) synth "$@" remix -m "${vols%,}" fade l "$attack" "$len" "$decay"
+done
