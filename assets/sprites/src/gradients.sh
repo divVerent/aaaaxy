f082bd7b747c2237d97885b6f96207d88b88538f
Rudolf Polzer <rpolzer@google.com>
2021-04-20 21:04:36 -0400

    Fix gradient pic colorspace.

---
+++ assets/sprites/src/gradients.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+convert -size 128x128 gradient:black-white -colorspace sRGB -depth 8 ../gradient_top_down.png
+convert -size 128x128 gradient:black-white -rotate 270 -colorspace sRGB -depth 8 ../gradient_left_right.png
+convert -size 128x128 radial-gradient:white-black -colorspace sRGB -depth 8 ../gradient_outside_inside.png
