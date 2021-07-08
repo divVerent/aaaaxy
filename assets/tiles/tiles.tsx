38b416cf345d339304312de26f1073ebb33e6855
Rudolf Polzer <rpolzer@google.com>
2021-07-08 14:08:20 -0700

    Let's just have one Wang set.

--- assets/tiles/tiles.tsx
+++ assets/tiles/tiles.tsx
@@ -2318,41 +2318,6 @@
   <image width="16" height="16" source="bgfinal_f.png"/>
  </tile>
  <wangsets>
-  <wangset name="Terrains" type="corner" tile="-1">
-   <wangcolor name="Block" color="#ff0000" tile="9" probability="1"/>
-   <wangcolor name="Open" color="#00ff00" tile="63" probability="1"/>
-   <wangtile tileid="13" wangid="0,2,0,2,0,1,0,2"/>
-   <wangtile tileid="17" wangid="0,2,0,1,0,2,0,2"/>
-   <wangtile tileid="21" wangid="0,2,0,1,0,1,0,2"/>
-   <wangtile tileid="24" wangid="0,2,0,2,0,2,0,1"/>
-   <wangtile tileid="29" wangid="0,2,0,2,0,1,0,1"/>
-   <wangtile tileid="31" wangid="0,1,0,2,0,2,0,2"/>
-   <wangtile tileid="35" wangid="0,1,0,2,0,2,0,1"/>
-   <wangtile tileid="39" wangid="0,1,0,1,0,2,0,2"/>
-   <wangtile tileid="45" wangid="0,2,0,1,0,2,0,1"/>
-   <wangtile tileid="47" wangid="0,2,0,1,0,1,0,1"/>
-   <wangtile tileid="50" wangid="0,1,0,2,0,1,0,2"/>
-   <wangtile tileid="51" wangid="0,1,0,2,0,1,0,1"/>
-   <wangtile tileid="53" wangid="0,1,0,1,0,2,0,1"/>
-   <wangtile tileid="54" wangid="0,1,0,1,0,1,0,2"/>
-   <wangtile tileid="55" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="56" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="57" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="58" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="59" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="60" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="61" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="62" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="63" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="283" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="284" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="285" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="286" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="287" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="288" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="289" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="290" wangid="0,2,0,2,0,2,0,2"/>
-  </wangset>
   <wangset name="Block/Open" type="mixed" tile="-1">
    <wangcolor name="Block" color="#000080" tile="-1" probability="1"/>
    <wangcolor name="Open" color="#ffff80" tile="-1" probability="1"/>
