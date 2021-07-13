59c60bb2a1f3345a0b04fee41cf28d97fefbc01b
Rudolf Polzer <rpolzer@google.com>
2021-07-13 13:21:21 -0700

    Also detect alpha being not 0 or 1.

--- scripts/audit_images.sh
+++ scripts/audit_images.sh
@@ -13,13 +13,20 @@ find .. -name \*.png | sort | while read -r file; do
 	esac
 	f=$(
 		convert \
-			\( "$file" -depth 8 -alpha off \) \
-			\( "$file" -depth 8 -alpha off +dither -remap cga_palette.pnm \) \
-			-channel RGB \
+			\( \
+				"$file" -depth 8 -alpha off \
+			\) \
+			\( \
+				"$file" -depth 8 -alpha off +dither \
+				-channel RGB -remap cga_palette.pnm \
+				-channel A -threshold 50% \
+				+channel \
+			\) \
+			-channel RGBA \
 			-metric RMSE -format '%[distortion]\n' -compare \
 			INFO:
 	)
 	if [ "$f" !=  0 ]; then
-		echo "convert \( '$file' -depth 8 -alpha off +dither -remap cga_palette.pnm \) \( '$file' -depth 8 -alpha extract \) -compose CopyOpacity -composite "$file"  # $f"
+		echo "convert \( '$file' -depth 8 -alpha off +dither -remap cga_palette.pnm \) \( '$file' -depth 8 -alpha extract -threshold 50% \) -compose CopyOpacity -composite "$file"  # $f"
 	fi
 done
