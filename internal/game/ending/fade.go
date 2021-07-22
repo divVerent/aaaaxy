173f3dee644fec1d090433505ef9b6d56bb64720
Rudolf Polzer <rpolzer@google.com>
2021-07-21 20:56:53 -0400

    Explain how we want to do the fade effect.

--- internal/game/ending/fade.go
+++ internal/game/ending/fade.go
@@ -22,17 +22,38 @@ import (
 // FadeTarget fades the screen out.
 type FadeTarget struct {
 	World *engine.World
+
+	Color1, Color2, Color3 [3]float64
 }
 
 func (f *FadeTarget) Spawn(w *engine.World, sp *level.Spawnable, e *engine.Entity) error {
 	f.World = w
-	// Note: duration, map_to_black, map_to_white.
+	// Note: duration, color_1, color_2, color_3.
+	// Precompute from this a base color and the vector to cancel (cross product of color diffs).
 	return nil
 }
 
 func (f *FadeTarget) Despawn() {}
 
-func (f *FadeTarget) Update() {}
+func (f *FadeTarget) Update() {
+	a := 0.5 // Fraction of time passed.
+	f := 1.0 / (1.0 - a)
+
+	// Find color matrix so that:
+	// - f = 0 maps Color1, Color2, Color3 to themselves, and all other colors to their plane.
+	// - f = 1 is identity.
+	// - Rest follows linearly.
+	// So, form of matrix is:
+	// M(x-A) + A
+	// where
+	// n = normalize((B-A) cross (C-A))
+	//
+	// Mx = x - (1-f)*n*dot(n, x)
+	// M = id - (1-f)*n*n^T
+	// T = A - MA
+
+	// Once we have this, we can make the global ColorM an entity property, collect all entity ColorMs in a prepass, and then apply them to all object and world rendering.
+}
 
 func (f *FadeTarget) SetState(originator, predecessor *engine.Entity, state bool) {
 	// TODO implement.
