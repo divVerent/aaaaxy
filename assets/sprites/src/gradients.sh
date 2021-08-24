fe39ba163c62799cc5376b35aa1db4d62f343a8f
Rudolf Polzer <divVerent@gmail.com>
2021-08-24 09:52:23 -0700

    Infinite keyboard: make it clearer.

--- assets/sprites/src/gradients.sh
+++ assets/sprites/src/gradients.sh
@@ -16,3 +16,11 @@
 convert -size 128x128 gradient:black-white -colorspace sRGB -depth 8 -alpha copy ../gradient_top_bottom.png
 convert -size 128x128 gradient:black-white -rotate 270 -colorspace sRGB -depth 8 -alpha copy ../gradient_left_right.png
 convert -size 128x128 radial-gradient:white-black -colorspace sRGB -depth 8 -alpha copy ../gradient_outside_inside.png
+
+for i in $(seq 0 7); do
+	convert \
+		\( -size 128x128 xc:black -colorspace gray +noise Random -gaussian-blur 2x2 -level 40%,60% \) \
+		\( ../gradient_outside_inside.png -alpha extract \) \
+		-compose CopyOpacity -composite \
+		../magic_idle_$i.png
+done
