062682baed1c08b75b9f5db61a13004f858db83d
Rudolf Polzer <divVerent@gmail.com>
2021-08-25 05:04:06 -0700

    Slightly clean up asset rebuilding.

---
+++ scripts/rebuild-assets.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script rebuilds all regeneratable assets.
+#
+# Ideally these assets should move into assets/generated and thus no longer be
+# in the git repo; will be some work to support this in Tiled.
+
+set -ex
+
+for src in "$(pwd)"/assets/*/src/*.sh; do
+	cd "${src%/*}"
+	sh "$src"
+done
