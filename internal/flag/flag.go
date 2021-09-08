92cf713c986941d54e552592ae7b31cc810b1d82
Rudolf Polzer <divVerent@gmail.com>
2021-09-08 13:20:40 -0700

    Fix license header replace mistake.

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -1,6 +1,6 @@
 // Copyright 2021 Google LLC
 //
-// Licensed under the Apache License, SaveGameVersion 2.0 (the "License");
+// Licensed under the Apache Livense, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
