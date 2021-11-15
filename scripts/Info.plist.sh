04d9c71d4a8fc8dcab45a5a2aac7484b99d437fe
Rudolf Polzer <rpolzer@google.com>
2021-11-15 10:10:07 -0500

    More typo fixing in OS X build.

--- scripts/Info.plist.sh
+++ scripts/Info.plist.sh
@@ -15,6 +15,7 @@
 
 version=$1
 
+cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -33,3 +34,4 @@ version=$1
 	<true/>
 </dict>
 </plist>
+EOF
