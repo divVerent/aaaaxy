5708b8fec01eabc7592690f1e27ef3da2592a799
Rudolf Polzer <rpolzer@google.com>
2022-02-28 11:50:41 -0500

    Fix another issue in the flatpak yml updating.

--- scripts/go-vendor-to-flatpak-yml.sh
+++ scripts/go-vendor-to-flatpak-yml.sh
@@ -56,6 +56,7 @@ while read -r command pkg ver _ replacementpkg replacementver; do
 			# Cut off subdirectory paths.
 			# go-gl seems to use that.
 			pkg=$(echo "$pkg" | cut -d / -f 1-3)
+			dir=$(echo "$dir" | cut -d / -f 1-3)
 			;;
 	esac
 	url=https://$pkg
