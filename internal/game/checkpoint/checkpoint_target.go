0aa8fd44bd997676ec65536951bc384544949b88
Rudolf Polzer <divVerent@gmail.com>
2022-03-15 04:58:29 -0700

    Show game save errors when hitting a checkpoint.
    More improvement for issue #19.

--- internal/game/checkpoint/checkpoint_target.go
+++ internal/game/checkpoint/checkpoint_target.go
@@ -105,6 +105,8 @@ func (c *CheckpointTarget) SetState(originator, predecessor *engine.Entity, stat
 	err := c.World.Save()
 	if err != nil {
 		log.Errorf("could not save game: %v", err)
+		str := fmt.Sprintf("Error:\ncould not save game:\n%v", err)
+		centerprint.New(fun.FormatText(&c.World.PlayerState, str), centerprint.Important, centerprint.Top, centerprint.NormalFont(), color.NRGBA{R: 255, G: 85, B: 85, A: 255}, 5*time.Second).SetFadeOut(true)
 		return
 	}
 	if c.Text != "" {
