b2680a4c40c97f4d324d7d1ee4df7d2d6aaecbc7
Rudolf Polzer <rpolzer@google.com>
2022-05-19 07:10:55 -0400

    Allow turning off license text inclusion by passing BUILDTAGS=noembedlicenses.
    Helps with #22, but keeping open to also fix the underlying issue with globbing on FreeBSD.

--- licenses/embed.go
+++ licenses/embed.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build embed
-// +build embed
+//go:build embed && !noembedlicenses
+// +build embed,!noembedlicenses
 
 package licenses
 
