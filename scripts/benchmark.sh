c4142435ed3b6cf9b8cb8bda8c3a33cfc68ccf48
Rudolf Polzer <rpolzer@google.com>
2022-07-14 19:39:39 -0400

    Run benchmarks n times.
    Can then e.g. compute a median with a pivot table in Sheets.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -16,24 +16,27 @@
 # Simple script to run a benchmark demo at all graphics levels.
 
 if [ $# -lt 2 ]; then
-	echo >&2 "Usage: $0 benchmark.dem binary with flags..."
+	echo >&2 "Usage: $0 benchmark.dem count binary with flags..."
 	exit 1
 fi
 
-demo=$1
+demo=$1; shift
+count=$1; shift
 
 run() {
 	tag=$1; shift
-	time=$(sh scripts/measure-timedemo.sh "$@" -load_config=false)
+	time=$(sh scripts/measure-timedemo.sh "$demo" "$@" -load_config=false)
 	if [ $? -eq 0 ]; then
 		echo "$tag,$time"
 	fi
 }
 
 echo "settings,start_time_sec,play_time_sec,quit_time_sec,total_time_sec"
-run lowest "$@" -palette=none -draw_blurs=false -draw_outside=false -expand_using_vertices_accurately=false -screen_filter=nearest
-run low    "$@" -palette=none -draw_blurs=false -draw_outside=false -expand_using_vertices_accurately=true  -screen_filter=nearest
-run medium "$@" -palette=none -draw_blurs=true  -draw_outside=false -expand_using_vertices_accurately=true  -screen_filter=simple
-run high   "$@" -palette=none -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=simple
-run max    "$@" -palette=none -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=linear2xcrt
-run vga    "$@" -palette=vga  -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=linear2xcrt
+for i in $(seq 1 "$count"); do
+	run lowest "$@" -palette=none -draw_blurs=false -draw_outside=false -expand_using_vertices_accurately=false -screen_filter=nearest
+	run low    "$@" -palette=none -draw_blurs=false -draw_outside=false -expand_using_vertices_accurately=true  -screen_filter=nearest
+	run medium "$@" -palette=none -draw_blurs=true  -draw_outside=false -expand_using_vertices_accurately=true  -screen_filter=simple
+	run high   "$@" -palette=none -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=simple
+	run max    "$@" -palette=none -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=linear2xcrt
+	run vga    "$@" -palette=vga  -draw_blurs=true  -draw_outside=true  -expand_using_vertices_accurately=true  -screen_filter=linear2xcrt
+done
