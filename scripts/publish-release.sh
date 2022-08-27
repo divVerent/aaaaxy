138856e3483c4cb4488972a62f3551597b47a7ce
Rudolf Polzer <rpolzer@google.com>
2022-08-27 11:22:17 -0400

    Remove docker container when done.

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -61,7 +61,7 @@ hub release create \
 	git fetch origin
 	git reset --hard origin/master
 	sed -i -e "s/^pkgver=.*/pkgver=${new#v}/; s/^pkgrel=.*/pkgrel=0/;" APKBUILD
-	docker run --mount=type=bind,source=$PWD,target=/aaaaxy alpine:edge /bin/sh -c '
+	docker run --rm --mount=type=bind,source=$PWD,target=/aaaaxy alpine:edge /bin/sh -c '
 		set -e
 		apk add alpine-sdk sudo
 		abuild-keygen -i -a -n
@@ -120,7 +120,7 @@ xdg-open 'https://flathub.org/builds/#/apps/io.github.divverent.aaaaxy'
 (
 	cd ../aur-aaaaxy
 	sed -i -e "s/^pkgver=.*/pkgver=${new#v}/; s/^pkgrel=.*/pkgrel=1/;" PKGBUILD
-	docker run --mount=type=bind,source=$PWD,target=/aaaaxy archlinux:latest /bin/sh -c '
+	docker run --rm --mount=type=bind,source=$PWD,target=/aaaaxy archlinux:latest /bin/sh -c '
 		set -e
 		pacman --noconfirm -Syu base-devel namcap pacman-contrib sudo
 		cd /aaaaxy
