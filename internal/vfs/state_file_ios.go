6a472e8887860a5a49dbc04c96edde8afe3c87d9
Rudolf Polzer <divVerent@gmail.com>
2023-04-11 17:11:46 -0700

    Fix iOS path processing.

--- internal/vfs/state_file_ios.go
+++ internal/vfs/state_file_ios.go
@@ -18,7 +18,6 @@
 package vfs
 
 import (
-	"errors"
 	"fmt"
 	"path/filepath"
 	"unsafe"
@@ -60,7 +59,7 @@ func initAppSupportPath() {
 	if appSupportPath == "" {
 		appSupportPathCStr := C.app_support_path()
 		if appSupportPathCStr == nil {
-			return "", errors.New("could not find documents path")
+			panic("could not find documents path")
 		}
 		defer C.free(unsafe.Pointer(appSupportPathCStr))
 		appSupportPath = C.GoString(appSupportPathCStr)
