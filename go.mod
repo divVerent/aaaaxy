965fe80bd3abb0b4f2de3a8a75ed356a2abf236c
Rudolf Polzer <rpolzer@google.com>
2024-02-19 13:54:13 -0500

    Implement my own Android asset loading.
    This was the last gomobile reference.

--- go.mod
+++ go.mod
@@ -19,7 +19,6 @@ require (
 	github.com/ncruces/zenity v0.10.12
 	github.com/zachomedia/go-bdf v0.0.0-20220611021443-a3af701111be
 	golang.org/x/image v0.15.0
-	golang.org/x/mobile v0.0.0-20240213143359-d1f7d3436075
 	golang.org/x/sys v0.19.0
 	golang.org/x/text v0.14.0
 )
--- internal/vfs/assets_fs_zip_android.go
+++ internal/vfs/assets_fs_zip_android.go
@@ -17,14 +17,68 @@
 
 package vfs
 
+// Following code is copied from
+// golang.org/x/mobile/asset/asset_android.go
+// but changed to work with ebitenmobile.
+
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+#cgo LDFLAGS: -landroid
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+#include <jni.h>
+#include <stdlib.h>
+
+static AAssetManager* asset_manager_init(uintptr_t java_vm, uintptr_t jni_env, jobject ctx) {
+	JavaVM* vm = (JavaVM*)java_vm;
+	JNIEnv* env = (JNIEnv*)jni_env;
+
+	// Equivalent to:
+	//	assetManager = ctx.getResources().getAssets();
+	jclass ctx_clazz = (*env)->FindClass(env, "android/content/Context");
+	jmethodID getres_id = (*env)->GetMethodID(env, ctx_clazz, "getResources", "()Landroid/content/res/Resources;");
+	jobject res = (*env)->CallObjectMethod(env, ctx, getres_id);
+	jclass res_clazz = (*env)->FindClass(env, "android/content/res/Resources");
+	jmethodID getam_id = (*env)->GetMethodID(env, res_clazz, "getAssets", "()Landroid/content/res/AssetManager;");
+	jobject am = (*env)->CallObjectMethod(env, res, getam_id);
+
+	// Pin the AssetManager and load an AAssetManager from it.
+	am = (*env)->NewGlobalRef(env, am);
+	return AAssetManager_fromJava(env, am);
+}
+*/
+import "C"
+
 import (
+	"errors"
 	"io"
 	"io/fs"
 	"sync"
 	"time"
+	"unsafe"
 
-	"golang.org/x/mobile/asset"
+	"github.com/hajimehoshi/ebiten/v2/mobile"
+
+	"github.com/divVerent/aaaaxy/internal/log"
 )
+
+var assetOnce sync.Once
+
+// asset_manager is the asset manager of the app.
+var assetManager *C.AAssetManager
+
+func assetInit() {
+	err := mobile.RunOnJVM(func(vm, env, ctx uintptr) error {
+		assetManager = C.asset_manager_init(C.uintptr_t(vm), C.uintptr_t(env), C.jobject(ctx))
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("asset: %v", err)
+	}
+}
 
 type fileInfo int64
 
@@ -36,17 +90,24 @@ func (i fileInfo) IsDir() bool        { return false }
 func (i fileInfo) Sys() interface{}   { return nil }
 
 type reader struct {
-	f   asset.File
+	ptr *C.AAsset
 	mu  sync.Mutex
 	pos int64
 }
 
 func (r *reader) readAt(p []byte, off int64) (int, error) {
-	_, err := r.f.Seek(off, io.SeekStart)
-	if err != nil {
-		return 0, err
+	pos := int64(C.AAsset_seek(r.ptr, C.off_t(off), C.int(io.SeekStart)))
+	if pos < 0 {
+		return 0, errors.New("could not seek in aaaaxy.dat")
 	}
-	return r.f.Read(p)
+	n := int(C.AAsset_read(r.ptr, unsafe.Pointer(&p[0]), C.size_t(len(p))))
+	if n == 0 && len(p) > 0 {
+		return 0, io.EOF
+	}
+	if n < 0 {
+		return 0, errors.New("could not read from aaaaxy.dat")
+	}
+	return n, nil
 }
 
 func (r *reader) Read(p []byte) (int, error) {
@@ -66,23 +127,28 @@ func (r *reader) ReadAt(p []byte, off int64) (int, error) {
 func (r *reader) Stat() (fs.FileInfo, error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	size, err := r.f.Seek(0, io.SeekEnd)
-	if err != nil {
-		return nil, err
-	}
+	size := int64(C.AAsset_getLength(r.ptr))
 	return fileInfo(size), nil
 }
 
 func (r *reader) Close() error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	return r.f.Close()
+	C.AAsset_close(r.ptr)
+	return nil
 }
 
 func openAssetsZip() (*reader, error) {
-	f, err := asset.Open("aaaaxy.dat")
-	if err != nil {
-		return nil, err
+	assetOnce.Do(assetInit)
+	cname := C.CString("aaaaxy.dat")
+	defer C.free(unsafe.Pointer(cname))
+	mode := C.int(C.AASSET_MODE_RANDOM)
+	if *pinAssetsToRAM {
+		mode = C.AASSET_MODE_STREAMING
 	}
-	return &reader{f: f}, nil
+	ptr := C.AAssetManager_open(assetManager, cname, mode)
+	if ptr == nil {
+		return nil, errors.New("could not open aaaaxy.dat")
+	}
+	return &reader{ptr: ptr}, nil
 }
