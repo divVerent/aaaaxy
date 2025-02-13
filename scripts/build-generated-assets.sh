881cf9ec156802566a87c0bd87fee239e1419ca9
HashingPath <196891986+HashingPath@users.noreply.github.com>
2025-02-13 15:33:47 +0530

    Merge conflict fix

--- scripts/build-generated-assets.sh
+++ scripts/build-generated-assets.sh
@@ -34,7 +34,7 @@ if [ x"$AAAAXY_GENERATE_ASSETS" = x'true' ]; then
 				# Using |cat> instead of > because snapcraft for some reason doesn't allow using a regular > shell redirection with "go run".
 				${GO} run ${GO_FLAGS} github.com/divVerent/aaaaxy/cmd/dumpcps -level="$lname" |cat> "assets/generated/$lname.cp.dot"
 				grep -c . "assets/generated/$lname.cp.dot"
-				neato -Tjson assets/generated/$lname.cp.dot > assets/generated/$lname.cp.json
+				neato -Tjson "assets/generated/$lname.cp.dot" > "assets/generated/$lname.cp.json"
 				grep -c . "assets/generated/$lname.cp.json"
 				trap - EXIT
 			fi
