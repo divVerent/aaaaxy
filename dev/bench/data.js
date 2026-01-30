3db329d517677884c4cedaef0a5a5ef46d5c0f44
Rudolf Polzer <divVerent@gmail.com>
2026-01-30 07:01:10 -0500

    Order the graphs.

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -2,158 +2,138 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1769771246137,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
-    "Any% linux-amd64": [
-      {
-        "commit": {
-          "author": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "committer": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "distinct": true,
-          "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
-          "message": "Integrate with github-action-benchmark.",
-          "timestamp": "2026-01-30T05:55:57-05:00",
-          "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+    "Any% linux-amd64-current": [{
+      "commit": {
+        "author": {
+          "name": "Rudolf Polzer",
+          "username": "divVerent",
+          "email": "divVerent@gmail.com"
         },
-        "date": 1769770790045,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% linux-amd64",
-            "value": 72.13487444,
-            "unit": "Seconds"
-          }
-        ]
-      }
-    ],
-    "Any% windows-amd64": [
-      {
-        "commit": {
-          "author": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "committer": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "distinct": true,
-          "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
-          "message": "Integrate with github-action-benchmark.",
-          "timestamp": "2026-01-30T05:55:57-05:00",
-          "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+        "committer": {
+          "name": "Rudolf Polzer",
+          "username": "divVerent",
+          "email": "divVerent@gmail.com"
         },
-        "date": 1769770802444,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-amd64",
-            "value": 97.5029618,
-            "unit": "Seconds"
-          }
-        ]
-      }
-    ],
-    "Any% windows-386": [
-      {
-        "commit": {
-          "author": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "committer": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "distinct": true,
-          "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
-          "message": "Integrate with github-action-benchmark.",
-          "timestamp": "2026-01-30T05:55:57-05:00",
-          "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+        "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
+        "message": "Integrate with github-action-benchmark.",
+        "timestamp": "2026-01-30T08:29:22Z",
+        "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+      },
+      "date": 1769771240846,
+      "tool": "customSmallerIsBetter",
+      "benches": [{
+        "name": "Any% linux-amd64-current",
+        "value": 72.710359447,
+        "unit": "Seconds"
+      }]
+    }],
+    "100% linux-amd64": [{
+      "commit": {
+        "author": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
         },
-        "date": 1769770819702,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-386",
-            "value": 101.487191,
-            "unit": "Seconds"
-          }
-        ]
-      }
-    ],
-    "100% linux-amd64": [
-      {
-        "commit": {
-          "author": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "committer": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "distinct": true,
-          "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
-          "message": "Integrate with github-action-benchmark.",
-          "timestamp": "2026-01-30T05:55:57-05:00",
-          "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+        "committer": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
         },
-        "date": 1769770899364,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "100% linux-amd64",
-            "value": 225.276002323,
-            "unit": "Seconds"
-          }
-        ]
-      }
-    ],
-    "Any% linux-amd64-current": [
-      {
-        "commit": {
-          "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "divVerent@gmail.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "divVerent@gmail.com"
-          },
-          "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
-          "message": "Integrate with github-action-benchmark.",
-          "timestamp": "2026-01-30T08:29:22Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+        "distinct": true,
+        "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
+        "message": "Integrate with github-action-benchmark.",
+        "timestamp": "2026-01-30T05:55:57-05:00",
+        "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
+        "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+      },
+      "date": 1769770899364,
+      "tool": "customSmallerIsBetter",
+      "benches": [{
+        "name": "100% linux-amd64",
+        "value": 225.276002323,
+        "unit": "Seconds"
+      }]
+    }],
+    "Any% linux-amd64": [{
+      "commit": {
+        "author": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
         },
-        "date": 1769771240846,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% linux-amd64-current",
-            "value": 72.710359447,
-            "unit": "Seconds"
-          }
-        ]
-      }
-    ]
+        "committer": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
+        },
+        "distinct": true,
+        "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
+        "message": "Integrate with github-action-benchmark.",
+        "timestamp": "2026-01-30T05:55:57-05:00",
+        "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
+        "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+      },
+      "date": 1769770790045,
+      "tool": "customSmallerIsBetter",
+      "benches": [{
+        "name": "Any% linux-amd64",
+        "value": 72.13487444,
+        "unit": "Seconds"
+      }]
+    }],
+    "Any% windows-amd64": [{
+      "commit": {
+        "author": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
+        },
+        "committer": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
+        },
+        "distinct": true,
+        "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
+        "message": "Integrate with github-action-benchmark.",
+        "timestamp": "2026-01-30T05:55:57-05:00",
+        "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
+        "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+      },
+      "date": 1769770802444,
+      "tool": "customSmallerIsBetter",
+      "benches": [{
+        "name": "Any% windows-amd64",
+        "value": 97.5029618,
+        "unit": "Seconds"
+      }]
+    }],
+    "Any% windows-386": [{
+      "commit": {
+        "author": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
+        },
+        "committer": {
+          "email": "divVerent@gmail.com",
+          "name": "Rudolf Polzer",
+          "username": "divVerent"
+        },
+        "distinct": true,
+        "id": "b369e36d57fcbe255fc646f033cf94bec70c234a",
+        "message": "Integrate with github-action-benchmark.",
+        "timestamp": "2026-01-30T05:55:57-05:00",
+        "tree_id": "f6663aeb5500f3d1890e1054eff71c06a6408fb9",
+        "url": "https://github.com/divVerent/aaaaxy/commit/b369e36d57fcbe255fc646f033cf94bec70c234a"
+      },
+      "date": 1769770819702,
+      "tool": "customSmallerIsBetter",
+      "benches": [{
+        "name": "Any% windows-386",
+        "value": 101.487191,
+        "unit": "Seconds"
+      }]
+    }]
   }
 }
