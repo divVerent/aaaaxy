5a2f0354a38f28e4acf7908e87d188962133c317
github-action-benchmark <github@users.noreply.github.com>
2026-01-30 12:45:00 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for 21923fde7c5550178a2a197d30ce33e1ad2cba01

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769777032313,
+  "lastUpdate": 1769777100486,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -314,6 +314,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 108.9889513,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "divVerent@gmail.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "divVerent@gmail.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "21923fde7c5550178a2a197d30ce33e1ad2cba01",
+          "message": "Limit chart items to 90.",
+          "timestamp": "2026-01-30T07:41:15-05:00",
+          "tree_id": "cdeac86da1794e96a65f9e9bf6f1488101981f16",
+          "url": "https://github.com/divVerent/aaaaxy/commit/21923fde7c5550178a2a197d30ce33e1ad2cba01"
+        },
+        "date": 1769777089842,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 101.6488912,
             "unit": "Seconds"
           }
         ]
