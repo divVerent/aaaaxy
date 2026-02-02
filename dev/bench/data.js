891d1691c5506d3d7f73862d43c3dab2ff8ab175
github-action-benchmark <github@users.noreply.github.com>
2026-02-02 23:16:35 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for bf65c30461c4cdebc23cdd71d42f33050846100f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770043870928,
+  "lastUpdate": 1770074195871,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -571,6 +571,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 74.86834928,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "id": "bf65c30461c4cdebc23cdd71d42f33050846100f",
+          "message": "Ebitengine update.",
+          "timestamp": "2026-02-02T14:45:48Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/bf65c30461c4cdebc23cdd71d42f33050846100f"
+        },
+        "date": 1770074190199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 85.056502978,
             "unit": "Seconds"
           }
         ]
