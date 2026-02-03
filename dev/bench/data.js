e2303e99056945f10119c4198a80fd47ee73c12c
github-action-benchmark <github@users.noreply.github.com>
2026-02-03 18:57:58 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for 746f6e344dec8c8d136adb6fc8bc43068996f326

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770145072790,
+  "lastUpdate": 1770145077990,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -1228,6 +1228,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 101.0677947,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "746f6e344dec8c8d136adb6fc8bc43068996f326",
+          "message": "Retune alert thresholds.",
+          "timestamp": "2026-02-03T19:54:24+01:00",
+          "tree_id": "f0d15b52ac191bdbce92d35573d45aa669981a0e",
+          "url": "https://github.com/divVerent/aaaaxy/commit/746f6e344dec8c8d136adb6fc8bc43068996f326"
+        },
+        "date": 1770145069444,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 99.3876793,
             "unit": "Seconds"
           }
         ]
