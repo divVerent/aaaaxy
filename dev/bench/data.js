283f91b9e0980c18cc16e77b93c07b57b745b488
github-action-benchmark <github@users.noreply.github.com>
2026-02-06 12:33:23 +0000

    add Any% linux-amd64-current (customSmallerIsBetter) benchmark result for 33236efc3e86c2f68a1564943a276af97163f70c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770333267139,
+  "lastUpdate": 1770381203312,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -588,6 +588,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64-current",
             "value": 74.834411953,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "id": "33236efc3e86c2f68a1564943a276af97163f70c",
+          "message": "Also run go-current on every push.",
+          "timestamp": "2026-02-03T18:55:38Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/33236efc3e86c2f68a1564943a276af97163f70c"
+        },
+        "date": 1770381199334,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64-current",
+            "value": 78.476344003,
             "unit": "Seconds"
           }
         ]
