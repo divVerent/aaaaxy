1a31af21021b515116ea2c63208ce63ea5925979
github-action-benchmark <github@users.noreply.github.com>
2026-02-06 23:15:45 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for 258bbb71d9974bd3d3c7ab17ee016cffdf9f9903

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770419717566,
+  "lastUpdate": 1770419744842,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -2035,6 +2035,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 116.4880693,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "id": "258bbb71d9974bd3d3c7ab17ee016cffdf9f9903",
+          "message": "Increase alert threshold due to noise.",
+          "timestamp": "2026-02-06T19:10:57Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/258bbb71d9974bd3d3c7ab17ee016cffdf9f9903"
+        },
+        "date": 1770419736278,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 100.8640847,
             "unit": "Seconds"
           }
         ]
