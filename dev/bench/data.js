1e65a2b0743231aec9f90b9ce2142bae98b4f83c
github-action-benchmark <github@users.noreply.github.com>
2026-02-07 23:17:17 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for 258bbb71d9974bd3d3c7ab17ee016cffdf9f9903

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770470950710,
+  "lastUpdate": 1770506237735,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -1208,6 +1208,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 72.387031437,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "id": "258bbb71d9974bd3d3c7ab17ee016cffdf9f9903",
+          "message": "Increase alert threshold due to noise.",
+          "timestamp": "2026-02-06T19:10:57Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/258bbb71d9974bd3d3c7ab17ee016cffdf9f9903"
+        },
+        "date": 1770506232485,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 74.7448756,
             "unit": "Seconds"
           }
         ]
