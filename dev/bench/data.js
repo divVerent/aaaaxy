584dd47483a2b8dcfc3ad00cc6488a28c1392d01
github-action-benchmark <github@users.noreply.github.com>
2026-03-07 14:03:14 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for a24a3c55ffcbf53b5f66b1528d487ca85a8be009

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772892192390,
+  "lastUpdate": 1772892194269,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -4058,6 +4058,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 72.880572114,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a24a3c55ffcbf53b5f66b1528d487ca85a8be009",
+          "message": "Bump actions/download-artifact from 7 to 8 (#596)\n\nBumps [actions/download-artifact](https://github.com/actions/download-artifact) from 7 to 8.\n- [Release notes](https://github.com/actions/download-artifact/releases)\n- [Commits](https://github.com/actions/download-artifact/compare/v7...v8)\n\n---\nupdated-dependencies:\n- dependency-name: actions/download-artifact\n  dependency-version: '8'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-07T14:59:44+01:00",
+          "tree_id": "12e821b22cfd25e2fd541cb8893de1f93d38388d",
+          "url": "https://github.com/divVerent/aaaaxy/commit/a24a3c55ffcbf53b5f66b1528d487ca85a8be009"
+        },
+        "date": 1772892190051,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 71.824381463,
             "unit": "Seconds"
           }
         ]
