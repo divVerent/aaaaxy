6fb0896dc171bae7d5a7fae6b36f723095e3597e
github-action-benchmark <github@users.noreply.github.com>
2026-03-07 14:03:12 +0000

    add Any% windows-amd64 (customSmallerIsBetter) benchmark result for d7e6aca413925001e96c51081238ccad55f42ca9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772892171747,
+  "lastUpdate": 1772892192390,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -5471,6 +5471,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-amd64",
             "value": 97.253681,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7e6aca413925001e96c51081238ccad55f42ca9",
+          "message": "Bump actions/upload-artifact from 6 to 7 (#597)\n\nBumps [actions/upload-artifact](https://github.com/actions/upload-artifact) from 6 to 7.\n- [Release notes](https://github.com/actions/upload-artifact/releases)\n- [Commits](https://github.com/actions/upload-artifact/compare/v6...v7)\n\n---\nupdated-dependencies:\n- dependency-name: actions/upload-artifact\n  dependency-version: '7'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-07T14:59:30+01:00",
+          "tree_id": "52f6a650ca819540e71ba884e0a4d8323d2e6a91",
+          "url": "https://github.com/divVerent/aaaaxy/commit/d7e6aca413925001e96c51081238ccad55f42ca9"
+        },
+        "date": 1772892186031,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-amd64",
+            "value": 97.3107965,
             "unit": "Seconds"
           }
         ]
