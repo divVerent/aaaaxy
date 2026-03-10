69b20377719dafb55a2f4b41d02831d5484c2406
github-action-benchmark <github@users.noreply.github.com>
2026-03-10 06:16:36 +0000

    add 100% linux-amd64 (customSmallerIsBetter) benchmark result for 39ef13bc5b3a28a0e71b70f865e16f8cf31900f8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773123336615,
+  "lastUpdate": 1773123396393,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -1493,6 +1493,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "100% linux-amd64",
             "value": 230.675282974,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "39ef13bc5b3a28a0e71b70f865e16f8cf31900f8",
+          "message": "Add an explicit policy about AI use.\n\nNothing in it except what follows directly from the project license.",
+          "timestamp": "2026-03-10T07:11:41+01:00",
+          "tree_id": "964a328c11a3b7e3c41b5a8900409fc941bd79da",
+          "url": "https://github.com/divVerent/aaaaxy/commit/39ef13bc5b3a28a0e71b70f865e16f8cf31900f8"
+        },
+        "date": 1773123390832,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "100% linux-amd64",
+            "value": 222.027539053,
             "unit": "Seconds"
           }
         ]
