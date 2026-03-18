be09b1622be2538a1cacfcd535e3ed495ef294fd
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 06:24:16 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for 651dd43b7d33c1edc5c9ec7802305790c0f6487d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773815006243,
+  "lastUpdate": 1773815056130,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -9104,6 +9104,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 105.4479063,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "651dd43b7d33c1edc5c9ec7802305790c0f6487d",
+          "message": "Don't actually need sed for this.",
+          "timestamp": "2026-03-18T07:20:00+01:00",
+          "tree_id": "e41dd673990b7d759a6805a0fb785f065ab08e53",
+          "url": "https://github.com/divVerent/aaaaxy/commit/651dd43b7d33c1edc5c9ec7802305790c0f6487d"
+        },
+        "date": 1773815048690,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 99.3957475,
             "unit": "Seconds"
           }
         ]
