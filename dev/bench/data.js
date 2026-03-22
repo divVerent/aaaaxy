2f8157ecc3b43e7a80e13197a584b00efc5c43fe
github-action-benchmark <github@users.noreply.github.com>
2026-03-22 10:51:03 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for 485aabcad0f89ddd4d6f16f5146d4861ba37a633

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774134878290,
+  "lastUpdate": 1774176663306,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -5801,6 +5801,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 72.445361176,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "divVerent@gmail.com",
+            "name": "divVerent",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "485aabcad0f89ddd4d6f16f5146d4861ba37a633",
+          "message": "Add a setting for the strength of screen flash effects. (#602)\n\nAlso, show \"off\" instead of \"0%\" for volume and screen flashes.\n\nFixes #601.",
+          "timestamp": "2026-03-22T11:48:33+01:00",
+          "tree_id": "df1cf96da45cde4eae0b7bef25b826aa9f022523",
+          "url": "https://github.com/divVerent/aaaaxy/commit/485aabcad0f89ddd4d6f16f5146d4861ba37a633"
+        },
+        "date": 1774176658341,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 72.524345539,
             "unit": "Seconds"
           }
         ]
