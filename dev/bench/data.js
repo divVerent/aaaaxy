e9138f76d4935b8cac59a51ba572509a3b837856
github-action-benchmark <github@users.noreply.github.com>
2026-03-22 11:04:08 +0000

    add Any% linux-amd64-current (customSmallerIsBetter) benchmark result for 589182b567b311e2daaca5ad0c343e2507c560ec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774177295257,
+  "lastUpdate": 1774177448810,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -3894,6 +3894,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64-current",
             "value": 74.82723887,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "589182b567b311e2daaca5ad0c343e2507c560ec",
+          "message": "Actually use title case for Off too.",
+          "timestamp": "2026-03-22T11:56:39+01:00",
+          "tree_id": "af47a98b5d6cb7a46164be7112418216214bc66c",
+          "url": "https://github.com/divVerent/aaaaxy/commit/589182b567b311e2daaca5ad0c343e2507c560ec"
+        },
+        "date": 1774177445315,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64-current",
+            "value": 73.684286976,
             "unit": "Seconds"
           }
         ]
