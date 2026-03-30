fd6a9337ecd27c3d3154757612be864b12bc9bac
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 14:00:32 +0000

    add 100% linux-amd64 (customSmallerIsBetter) benchmark result for 6b6e215e9c388fa09498aca1a7d4839f8e57b043

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774879219374,
+  "lastUpdate": 1774879232936,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -2352,6 +2352,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "100% linux-amd64",
             "value": 217.396528997,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "43880903+transifex-integration[bot]@users.noreply.github.com",
+            "name": "transifex-integration[bot]",
+            "username": "transifex-integration[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b6e215e9c388fa09498aca1a7d4839f8e57b043",
+          "message": "Translate game.pot in zh-Hant [Manual Sync] (#623)\n\n98% of minimum 97% reviewed source file: 'game.pot'\non 'zh-Hant'.\n\nSync of partially translated files: \nuntranslated content is included with an empty translation \nor source language content depending on file format\n\nCo-authored-by: transifex-integration[bot] <43880903+transifex-integration[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-30T15:50:35+02:00",
+          "tree_id": "5154b3f53754b0b61e2ba94e02ddd7664a9f004d",
+          "url": "https://github.com/divVerent/aaaaxy/commit/6b6e215e9c388fa09498aca1a7d4839f8e57b043"
+        },
+        "date": 1774879222222,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "100% linux-amd64",
+            "value": 216.782500861,
             "unit": "Seconds"
           }
         ]
