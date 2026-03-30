4e37bb779833be6d192ef52d9344eaf418f38a08
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 14:38:46 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for db408381ae8e2404413527dfeb80c6b577e92fab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774881523465,
+  "lastUpdate": 1774881526108,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -10020,35 +10020,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "committer": {
-            "email": "divVerent@gmail.com",
-            "name": "Rudolf Polzer",
-            "username": "divVerent"
-          },
-          "distinct": true,
-          "id": "21923fde7c5550178a2a197d30ce33e1ad2cba01",
-          "message": "Limit chart items to 90.",
-          "timestamp": "2026-01-30T07:41:15-05:00",
-          "tree_id": "cdeac86da1794e96a65f9e9bf6f1488101981f16",
-          "url": "https://github.com/divVerent/aaaaxy/commit/21923fde7c5550178a2a197d30ce33e1ad2cba01"
-        },
-        "date": 1769777089842,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-386",
-            "value": 101.6488912,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "rpolzer@google.com",
             "name": "Rudolf Polzer",
             "username": "divVerent"
@@ -12515,6 +12486,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 99.5141935,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "43880903+transifex-integration[bot]@users.noreply.github.com",
+            "name": "transifex-integration[bot]",
+            "username": "transifex-integration[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db408381ae8e2404413527dfeb80c6b577e92fab",
+          "message": "Translate game.pot in be@tarask [Manual Sync] (#620)\n\n98% of minimum 97% reviewed source file: 'game.pot'\non 'be@tarask'.\n\nSync of partially translated files: \nuntranslated content is included with an empty translation \nor source language content depending on file format\n\nCo-authored-by: transifex-integration[bot] <43880903+transifex-integration[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-30T16:10:25+02:00",
+          "tree_id": "a713f594bdc984bd8757fffcd1445b1e889ae862",
+          "url": "https://github.com/divVerent/aaaaxy/commit/db408381ae8e2404413527dfeb80c6b577e92fab"
+        },
+        "date": 1774881517203,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 99.152947,
             "unit": "Seconds"
           }
         ]
