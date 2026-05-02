d962bce9f00c1a08bb6302f9b1c466e1328f11ee
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 12:04:03 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for 62bdb35abaf8c7107ad9d484a19134aa389b64ea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777723365956,
+  "lastUpdate": 1777723443952,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -5040,33 +5040,6 @@ window.BENCHMARK_DATA = {
           "timestamp": "2026-03-10T06:19:58Z",
           "url": "https://github.com/divVerent/aaaaxy/commit/b62e771d5753b7e5a783abefd0dc296a6d1749ea"
         },
-        "date": 1773270216468,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% linux-amd64",
-            "value": 74.648447664,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "id": "b62e771d5753b7e5a783abefd0dc296a6d1749ea",
-          "message": "Rephrase to make clear copyright applies to all works, not just code.",
-          "timestamp": "2026-03-10T06:19:58Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/b62e771d5753b7e5a783abefd0dc296a6d1749ea"
-        },
         "date": 1773357360575,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -7527,6 +7500,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 74.693670955,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "62bdb35abaf8c7107ad9d484a19134aa389b64ea",
+          "message": "Run strip-nondeterminism after advzip.\n\nShould not actually fix or break anything but is cleaner.",
+          "timestamp": "2026-05-02T14:01:19+02:00",
+          "tree_id": "cfaa36bf3ed690165241b8d8323ee7ae9f998c79",
+          "url": "https://github.com/divVerent/aaaaxy/commit/62bdb35abaf8c7107ad9d484a19134aa389b64ea"
+        },
+        "date": 1777723440077,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 70.931629392,
             "unit": "Seconds"
           }
         ]
