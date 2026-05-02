187bb353159da45d4aab795206db4423012ffa13
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 08:34:05 +0000

    add Any% linux-amd64 (customSmallerIsBetter) benchmark result for 32f0f34c55a82c767af8fb0f3aaa925c136e0e1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777710819098,
+  "lastUpdate": 1777710845537,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -5030,33 +5030,6 @@ window.BENCHMARK_DATA = {
           "timestamp": "2026-03-07T13:59:44Z",
           "url": "https://github.com/divVerent/aaaaxy/commit/a24a3c55ffcbf53b5f66b1528d487ca85a8be009"
         },
-        "date": 1773010956741,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% linux-amd64",
-            "value": 69.402359858,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "dependabot[bot]",
-            "username": "dependabot[bot]",
-            "email": "49699333+dependabot[bot]@users.noreply.github.com"
-          },
-          "committer": {
-            "name": "GitHub",
-            "username": "web-flow",
-            "email": "noreply@github.com"
-          },
-          "id": "a24a3c55ffcbf53b5f66b1528d487ca85a8be009",
-          "message": "Bump actions/download-artifact from 7 to 8 (#596)\n\nBumps [actions/download-artifact](https://github.com/actions/download-artifact) from 7 to 8.\n- [Release notes](https://github.com/actions/download-artifact/releases)\n- [Commits](https://github.com/actions/download-artifact/compare/v7...v8)\n\n---\nupdated-dependencies:\n- dependency-name: actions/download-artifact\n  dependency-version: '8'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
-          "timestamp": "2026-03-07T13:59:44Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/a24a3c55ffcbf53b5f66b1528d487ca85a8be009"
-        },
         "date": 1773098156892,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -7511,6 +7484,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64",
             "value": 72.625732986,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "32f0f34c55a82c767af8fb0f3aaa925c136e0e1a",
+          "message": "Bump apple-actions/upload-testflight-build from 3 to 5 (#636)\n\nBumps [apple-actions/upload-testflight-build](https://github.com/apple-actions/upload-testflight-build) from 3 to 5.\n- [Release notes](https://github.com/apple-actions/upload-testflight-build/releases)\n- [Commits](https://github.com/apple-actions/upload-testflight-build/compare/v3...v5)\n\n---\nupdated-dependencies:\n- dependency-name: apple-actions/upload-testflight-build\n  dependency-version: '5'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-05-02T10:28:21+02:00",
+          "tree_id": "e75cf2b104d5deb2d337890f918034fc7e9ad83c",
+          "url": "https://github.com/divVerent/aaaaxy/commit/32f0f34c55a82c767af8fb0f3aaa925c136e0e1a"
+        },
+        "date": 1777710840709,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64",
+            "value": 55.016936042,
             "unit": "Seconds"
           }
         ]
