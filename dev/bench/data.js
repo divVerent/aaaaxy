f7310b2c23438011b952f30d396716d785d34035
github-action-benchmark <github@users.noreply.github.com>
2026-06-15 19:15:31 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for df7e2a67974a05d343fb2a513672528b79d8b1a3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781550912325,
+  "lastUpdate": 1781550931640,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -9990,33 +9990,6 @@ window.BENCHMARK_DATA = {
           "timestamp": "2026-04-08T12:05:58Z",
           "url": "https://github.com/divVerent/aaaaxy/commit/1fc2e8fa0828f24c7b162005713d8a98867d1559"
         },
-        "date": 1776123793753,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-386",
-            "value": 99.6286484,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "id": "1fc2e8fa0828f24c7b162005713d8a98867d1559",
-          "message": "Release v1.7.50\n\nChanges since v1.7.12:\n  - Menu: add a setting for the strength of screen flash effects. (#602)",
-          "timestamp": "2026-04-08T12:05:58Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/1fc2e8fa0828f24c7b162005713d8a98867d1559"
-        },
         "date": 1776209714548,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -12459,6 +12432,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 104.3893112,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "df7e2a67974a05d343fb2a513672528b79d8b1a3",
+          "message": "Give the spike meme music.",
+          "timestamp": "2026-06-15T21:11:19+02:00",
+          "tree_id": "ede7eae978998b787759eb06f5fe7b7e8f5289ea",
+          "url": "https://github.com/divVerent/aaaaxy/commit/df7e2a67974a05d343fb2a513672528b79d8b1a3"
+        },
+        "date": 1781550924639,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 99.7572362,
             "unit": "Seconds"
           }
         ]
