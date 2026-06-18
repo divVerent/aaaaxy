8e64f4eaeebadf2a9dd70a9f7fbcfaed48ae2b22
github-action-benchmark <github@users.noreply.github.com>
2026-06-18 00:06:49 +0000

    add Any% windows-amd64 (customSmallerIsBetter) benchmark result for f1915ff40f7cc55ac23bcad1324fe2b3bb970223

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781741196114,
+  "lastUpdate": 1781741209675,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -7502,33 +7502,6 @@ window.BENCHMARK_DATA = {
           "timestamp": "2026-04-18T06:13:39Z",
           "url": "https://github.com/divVerent/aaaaxy/commit/04f5670b63bf0117c0444f5e2d87dd37cfc2c9da"
         },
-        "date": 1776641133075,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-amd64",
-            "value": 98.5924385,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "id": "04f5670b63bf0117c0444f5e2d87dd37cfc2c9da",
-          "message": "Modules update.",
-          "timestamp": "2026-04-18T06:13:39Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/04f5670b63bf0117c0444f5e2d87dd37cfc2c9da"
-        },
         "date": 1776727945696,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -9973,6 +9946,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-amd64",
             "value": 96.9386354,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "rpolzer@google.com"
+          },
+          "id": "f1915ff40f7cc55ac23bcad1324fe2b3bb970223",
+          "message": "Add a delay.",
+          "timestamp": "2026-06-15T19:16:34Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/f1915ff40f7cc55ac23bcad1324fe2b3bb970223"
+        },
+        "date": 1781741201925,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-amd64",
+            "value": 97.5988781,
             "unit": "Seconds"
           }
         ]
