7e3a2a2b450eadb7ffd925e11e0087f9010b35b2
github-action-benchmark <github@users.noreply.github.com>
2026-07-05 19:11:58 +0000

    add Any% linux-amd64-current (customSmallerIsBetter) benchmark result for c3e5efdce9559b611cc7739dbf93cd246b0e5786

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783278521573,
+  "lastUpdate": 1783278718672,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -2504,33 +2504,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "id": "62bdb35abaf8c7107ad9d484a19134aa389b64ea",
-          "message": "Run strip-nondeterminism after advzip.\n\nShould not actually fix or break anything but is cleaner.",
-          "timestamp": "2026-05-02T12:01:19Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/62bdb35abaf8c7107ad9d484a19134aa389b64ea"
-        },
-        "date": 1778417462787,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% linux-amd64-current",
-            "value": 78.237331719,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "rpolzer@google.com",
             "name": "Rudolf Polzer",
             "username": "divVerent"
@@ -4993,6 +4966,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% linux-amd64-current",
             "value": 73.151988256,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "committer": {
+            "email": "rpolzer@google.com",
+            "name": "Rudolf Polzer",
+            "username": "divVerent"
+          },
+          "distinct": true,
+          "id": "c3e5efdce9559b611cc7739dbf93cd246b0e5786",
+          "message": "Let's reference Beethoven's red scarf with the colors.",
+          "timestamp": "2026-07-05T21:03:43+02:00",
+          "tree_id": "d5b2ce78128d2132a9c59dba9e7c194ac193af1e",
+          "url": "https://github.com/divVerent/aaaaxy/commit/c3e5efdce9559b611cc7739dbf93cd246b0e5786"
+        },
+        "date": 1783278714366,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% linux-amd64-current",
+            "value": 72.724940947,
             "unit": "Seconds"
           }
         ]
