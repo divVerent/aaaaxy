86d144d600a906bf255e846c9bea2eddc4db4b72
github-action-benchmark <github@users.noreply.github.com>
2026-07-08 23:57:14 +0000

    add Any% windows-386 (customSmallerIsBetter) benchmark result for 05646e6444394b7e06d4c3d78c516fc2e6a794d6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783555012962,
+  "lastUpdate": 1783555033812,
   "repoUrl": "https://github.com/divVerent/aaaaxy",
   "entries": {
     "100% linux-amd64": [
@@ -10148,33 +10148,6 @@ window.BENCHMARK_DATA = {
           "timestamp": "2026-05-25T15:47:25Z",
           "url": "https://github.com/divVerent/aaaaxy/commit/fdbe00cdb8c9bb43e20f0e179f28eda6989402ad"
         },
-        "date": 1779925992970,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Any% windows-386",
-            "value": 103.0543443,
-            "unit": "Seconds"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "committer": {
-            "name": "Rudolf Polzer",
-            "username": "divVerent",
-            "email": "rpolzer@google.com"
-          },
-          "id": "fdbe00cdb8c9bb43e20f0e179f28eda6989402ad",
-          "message": "Fix sorting.",
-          "timestamp": "2026-05-25T15:47:25Z",
-          "url": "https://github.com/divVerent/aaaaxy/commit/fdbe00cdb8c9bb43e20f0e179f28eda6989402ad"
-        },
         "date": 1780012817333,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -12657,6 +12630,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "Any% windows-386",
             "value": 99.8117276,
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "divVerent@gmail.com"
+          },
+          "committer": {
+            "name": "Rudolf Polzer",
+            "username": "divVerent",
+            "email": "divVerent@gmail.com"
+          },
+          "id": "05646e6444394b7e06d4c3d78c516fc2e6a794d6",
+          "message": "Expire all centerprints when an important centerprint spawns.\n\nMakes the easter eggs a lot nicer and no longer with limited time to\nread.",
+          "timestamp": "2026-07-08T17:12:23Z",
+          "url": "https://github.com/divVerent/aaaaxy/commit/05646e6444394b7e06d4c3d78c516fc2e6a794d6"
+        },
+        "date": 1783555025983,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Any% windows-386",
+            "value": 99.5981523,
             "unit": "Seconds"
           }
         ]
